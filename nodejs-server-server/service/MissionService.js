c1299001cd96e24f5801e904fe63b81e31ac0f24
ethanyang <chuunibyohan@gmail.com>
2020-11-12 00:22:49 +0100

    update

--- nodejs-server-server/service/MissionService.js
+++ nodejs-server-server/service/MissionService.js
@@ -11,22 +11,24 @@ var ObjectId = require('mongodb').ObjectId;
  **/
 exports.createMission = function (body) {
   return new Promise(function (resolve, reject) {
-    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
       if (err) throw err;
       var dbo = db.db("greenhero");
-      var myobj = { "title": body.title,
-                    "description": body.description,
-                    "value": body.value,
-                    "goal": body.goal,
-                    "reward": body.reward,
-                    "image": body.image,
-                    "deleted": body.deleted,
-                    "type": body.type};
-      dbo.collection("Mission").insertOne(myobj, function(err, res) {
-          if (err) throw err;
-          console.log("successful");
-          resolve(res);
-          db.close();
+      var myobj = {
+        "title": body.title,
+        "description": body.description,
+        "value": body.value,
+        "goal": body.goal,
+        "reward": body.reward,
+        "image": body.image,
+        "deleted": body.deleted,
+        "type": body.type
+      };
+      dbo.collection("Mission").insertOne(myobj, function (err, res) {
+        if (err) throw err;
+        console.log("successful");
+        resolve(res);
+        db.close();
       });
     });
   });
@@ -42,17 +44,17 @@ exports.createMission = function (body) {
  **/
 exports.deleteMissionById = function (_id) {
   return new Promise(function (resolve, reject) {
-    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
       if (err) throw err;
       var dbo = db.db("greenhero");
-      var whereStr = {"_id":ObjectId(_id)};  // condition
-      dbo.collection("Mission").deleteOne(whereStr, function(err, obj) {
-          if (err) throw err;
-          console.log("successful");
-          resolve();
-          db.close();
+      var whereStr = { "_id": ObjectId(_id) };  // condition
+      dbo.collection("Mission").deleteOne(whereStr, function (err, obj) {
+        if (err) throw err;
+        console.log("successful");
+        resolve();
+        db.close();
       });
-  });
+    });
   });
 }
 
@@ -63,7 +65,7 @@ exports.deleteMissionById = function (_id) {
  *
  * returns List
  **/
-exports.getAllMissions = function () {
+function getAllMissions() {
   return new Promise(function (resolve, reject) {
     MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
       if (err) throw err;
@@ -98,6 +100,141 @@ exports.getAllMissions = function () {
     }];
   });
 }
+exports.getAllMissions = getAllMissions;
+
+/**
+ * Get mission
+ * See the available missions
+ *
+ * title String The title of the mission.
+ * type String The type of the mission.
+ * returns Mission
+ **/
+exports.getMissionByTitleAndType = function (title, type) {
+  return new Promise(function (resolve, reject) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("greenhero");
+      dbo.collection("Mission").find({ "title": title, "type": type }).toArray(function (err, result) {
+        if (err) throw err;
+        resolve(result);
+        db.close();
+      });
+    });
+  });
+}
+
+/**
+ * Get mission
+ * See the available missions
+ *
+ * title String The title of the mission.
+ * type String The type of the mission.
+ * returns Mission
+ **/
+exports.getMissionByUserIdAndType = function (user_id, type) {
+  return new Promise(function (resolve, reject) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("greenhero");
+      dbo.collection("Mission").find({
+        "user_id": ObjectId(user_id),
+        "type": type
+      }).toArray(function (err, result) {
+        if (err) throw err;
+        resolve(result);
+        db.close();
+      });
+    });
+  });
+}
+
+/**
+ * Get mission
+ * See the available missions
+ *
+ * title String The title of the mission.
+ * type String The type of the mission.
+ * returns Mission
+ **/
+exports.getTopMission = function (date) {
+  return new Promise(function (resolve, reject) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("greenhero");
+      dbo.collection("Mission").find({
+        "openDate": {
+          $gte: date
+        }
+      }).toArray(function (err, result) {
+        if (err) throw err;
+        let missions = {};
+        for(let mission of result) {
+          if (Object.keys(missions).some((key) => key === mission.title)){
+            missions[mission.title] += 1;
+          }
+          else {
+            missions[mission.title] = 1;
+          }
+        }
+
+        var highestTotal = 0;
+        var highestCategory;
+        for(let key of Object.keys(missions)) {
+          if(missions[key] >= highestTotal) {
+            highestTotal = missions[key];
+            highestCategory = key;
+          }
+        }
+        resolve({res: highestCategory});
+        db.close();
+      });
+    });
+  });
+}
+
+/**
+ * Get mission
+ * See the available missions
+ *
+ * title String The title of the mission.
+ * type String The type of the mission.
+ * returns Mission
+ **/
+exports.getCompletedMissionByType = function (date) {
+  return new Promise(function (resolve, reject) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("greenhero");
+      dbo.collection("Mission").find({
+        "openDate": {
+          $gte: date
+        },
+        "completed": true
+      }).toArray(function (err, result) {
+        if (err) throw err;
+        let missions = {};
+        for(let mission of result) {
+          if (Object.keys(missions).some((key) => key === mission.title)){
+            missions[mission.title] += 1;
+          }
+          else {
+            missions[mission.title] = 1;
+          }
+        }
+        let res = []
+        for(let key of Object.keys(missions)) {
+          let obj = {}
+          obj["name"] = key;
+          obj["value"] = missions[key];
+          res.push(obj)
+        }
+        resolve(res);
+        db.close();
+      });
+    });
+  });
+}
 
 
 /**
@@ -108,20 +245,26 @@ exports.getAllMissions = function () {
  * type String The type of the mission.
  * returns Mission
  **/
-exports.getMissionByTitleAndType = function(title,type) {
-  return new Promise(function(resolve, reject) {
-    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+exports.getNumberofCompletedMission = function (date) {
+  return new Promise(function (resolve, reject) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
       if (err) throw err;
       var dbo = db.db("greenhero");
-      dbo.collection("Mission"). find({"title":title,"type":type}).toArray(function(err, result) { 
-          if (err) throw err;
-          resolve(result); 
-          db.close();
+      var num = 0;
+      dbo.collection("Mission").find({
+        "completed": true
+      }).toArray(async function (err, result) {
+        if (err) throw err;
+        num = result.length
+        let total_missions = await getAllMissions();
+        let total = total_missions.length
+        let res = Math.round(num / total * 100)
+        resolve({ total: res });
+        db.close();
       });
-          });
+    });
   });
 }
-
 
 /**
  * Modify mission
@@ -130,26 +273,30 @@ exports.getMissionByTitleAndType = function(title,type) {
  * body Mission Updated mission object
  * no response value expected for this operation
  **/
-exports.modifyMission = function(body) {
-  return new Promise(function(resolve, reject) {
-    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+exports.modifyMission = function (body) {
+  return new Promise(function (resolve, reject) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
       if (err) throw err;
       var dbo = db.db("greenhero");
-      var whereStr = {"_id":ObjectId(body._id)};  // condition
-      var updateStr = {$set: { "title" : body.title,
-                                "description" :body.description,
-                                "value" : body.value,
-                                "goal" : body.goal,
-                                "reward": body.reward,
-                                "image": body.image,
-                                "deleted": body.deleted,
-                                "type" :body.type}};
-      dbo.collection("Mission").updateOne(whereStr, updateStr, function(err, res) {
-          if (err) throw err;
-          console.log("successful");
-          db.close();
+      var whereStr = { "_id": ObjectId(body._id) };  // condition
+      var updateStr = {
+        $set: {
+          "title": body.title,
+          "description": body.description,
+          "value": body.value,
+          "goal": body.goal,
+          "reward": body.reward,
+          "image": body.image,
+          "deleted": body.deleted,
+          "type": body.type
+        }
+      };
+      dbo.collection("Mission").updateOne(whereStr, updateStr, function (err, res) {
+        if (err) throw err;
+        console.log("successful");
+        db.close();
       });
-  });
+    });
   });
 }
 
